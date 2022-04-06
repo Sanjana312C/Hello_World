326e037470b9181d6eeb0a4b6226721e24b71fc4
Sanjana312C <103070046+Sanjana312C@users.noreply.github.com>
2022-04-06 20:53:50 +0530

    app update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/howareyou', (req, res) => res.send('I am fine, Thank you'));
+app.get('/howareyou', (req, res) => res.send('I am fine, Thank you , How are you doing?'));
 
 app.listen(3000, () => {
     console.log('My API is running on port 3000!');
